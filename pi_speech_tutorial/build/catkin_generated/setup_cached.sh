ccc3be01784352927f3ecd994dc80b137f38d8f3
nj1407 <nathanjohn@utexas.edu>
2016-05-09 17:47:14 -0500

    final project

---
+++ pi_speech_tutorial/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/lib:/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/lib/x86_64-linux-gnu:/home/nathan/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/nathan/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/lib/pkgconfig:/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/lib/x86_64-linux-gnu/pkgconfig:/home/nathan/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/nathan/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nathan/catkin_ws/src/pi_speech_tutorial/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/nathan/catkin_ws/src/pi_speech_tutorial:$ROS_PACKAGE_PATH"
